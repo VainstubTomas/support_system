f27d776025792633a67482214b9dc893e87ae604
Tomas <vainstubtomas@gmail.com>
2024-08-07 10:59:24 -0300

    Created token generator

--- back_api/settings.py
+++ back_api/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 from pathlib import Path
 
@@ -29,8 +30,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'back',
     'corsheaders',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist'
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -42,6 +46,19 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+      ],
+}
+
+SIMPLE_JWT = {
+     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+     'ROTATE_REFRESH_TOKENS': True,
+     'BLACKLIST_AFTER_ROTATION': True
+}
 
 ROOT_URLCONF = 'back_api.urls'
 
--- back_api/urls.py
+++ back_api/urls.py
@@ -1,12 +1,16 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import login_view
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('back.urls', namespace='back_api')),
-    path('api/login/', login_view, name='login_view'),
-    path('',include('back.urls', namespace='back'))
+    path('',include('back.urls', namespace='back')),
+    path('token/', 
+          jwt_views.TokenObtainPairView.as_view(), 
+          name ='token_obtain_pair'),
+     path('token/refresh/', 
+          jwt_views.TokenRefreshView.as_view(), 
+          name ='token_refresh')
 ]
